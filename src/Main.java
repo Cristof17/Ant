9aefdb8de91c30e2b0ce691e01f6b7dce1893c47
Robert Rotsching <robert.rotsching@gmail.com>
2019-01-29 15:31:41 +0200

    Creating described objects

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import gnu.prolog.vm.Environment;
 import gnu.prolog.term.AtomTerm;
 import gnu.prolog.term.Term;
 import gnu.prolog.term.CompoundTerm;
+import gnu.prolog.database.PrologTextLoader;
+import gnu.prolog.database.PrologTextLoaderState;
 
 public class Main {
     public static void main(String[] args){
@@ -13,14 +15,19 @@ public class Main {
 
         /*
          * Add the clause to the database. Steps:
-         * -create an Environment object
+         * -create an Environment object with empty constructor
          * -create a PrologTextLoaderState object and pass the Environment
          *      object to the constructor of PrologTextLoaderState
          * -create a PrologTextLoader object and pass the PrologTextLoaderState
-         *      object to the constructor of PrologTextLoader
+         *      object to the constructor of PrologTextLoader and a null
+         *      reference to replace the Term object required by the constructor
+         *      which should be converted to Term to pass the compiler
+         *      "ambigous"
          * -call processClause on the PrologTextLoaderState object passing the
                 PrologTextLoader object and the Term object to be added
          */
-        
+        Environment env = new Environment();
+        PrologTextLoaderState state = new PrologTextLoaderState(env);
+        PrologTextLoader loader = new PrologTextLoader(state, (Term)null);
     }
 }
