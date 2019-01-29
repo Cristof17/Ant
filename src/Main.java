422b8b7788346736115868a6fe4258c768d08863
Robert Rotsching <robert.rotsching@gmail.com>
2019-01-29 14:43:53 +0200

    Created a simple rule

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 import gnu.prolog.vm.Environment;
+import gnu.prolog.term.AtomTerm;
+import gnu.prolog.term.Term;
+import gnu.prolog.term.CompoundTerm;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("HAHA");
+        Environment env = new Environment();
+        AtomTerm cristof = AtomTerm.get("cristof");
+        AtomTerm robert = AtomTerm.get("robert");
+        AtomTerm functor = AtomTerm.get("frate"); 
+        Term[] arguments = new Term[]{cristof, robert};
+        CompoundTerm regula = new CompoundTerm(functor, arguments);
     }
 }
