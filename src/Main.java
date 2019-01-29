eb176ccdd24def6e751ad80b3cbec86bd0241af7
Robert Rotsching <robert.rotsching@gmail.com>
2019-01-29 15:12:54 +0200

    Added steps to make in order to add a new rule to the database

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,22 @@ import gnu.prolog.term.CompoundTerm;
 
 public class Main {
     public static void main(String[] args){
-        Environment env = new Environment();
         AtomTerm cristof = AtomTerm.get("cristof");
         AtomTerm robert = AtomTerm.get("robert");
         AtomTerm functor = AtomTerm.get("frate"); 
         Term[] arguments = new Term[]{cristof, robert};
         CompoundTerm regula = new CompoundTerm(functor, arguments);
+
+        /*
+         * Add the clause to the database. Steps:
+         * -create an Environment object
+         * -create a PrologTextLoaderState object and pass the Environment
+         *      object to the constructor of PrologTextLoaderState
+         * -create a PrologTextLoader object and pass the PrologTextLoaderState
+         *      object to the constructor of PrologTextLoader
+         * -call processClause on the PrologTextLoaderState object passing the
+                PrologTextLoader object and the Term object to be added
+         */
+        
     }
 }
